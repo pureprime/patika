a911bb15a884d28e7b534a9adb51b61ab12f9b58
pureprime <93528417+pureprime@users.noreply.github.com>
2022-07-09 17:22:26 +0300

    Bittiş

---
+++ Linkedin/index.js
@@ -0,0 +1,11 @@
+function göster(){
+
+
+var y = document.getElementsByClassName("bi-chevron-compact-up")[0].style.opacity="1";
+
+setTimeout(function sil(){
+  var z = document.getElementsByClassName("bi-chevron-compact-up")[0].style.opacity="0";
+
+
+},1000);
+};
